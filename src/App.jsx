d4fd5fa950b06e8043c29d00e5961200f691a256
quantum0X <thevishal2108@gmail.com>
2023-02-19 00:59:35 +0530

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter , Route, Routes, Link} from "react-router-dom";
 // import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import CreatePost from "./pages/CreatePost";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <header className="bg-white py-4 px-8 shadow text-center">This is header.</header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreatePost/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
